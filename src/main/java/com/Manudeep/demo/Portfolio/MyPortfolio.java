5fe35f761a03d072c8f728995dd9cf665b3876fc
manudeep <manudeep1000@gmail.com>
2026-07-14 11:30:11 +0530

    Added my projects in my portfolio

--- src/main/java/com/Manudeep/demo/Portfolio/MyPortfolio.java
+++ src/main/java/com/Manudeep/demo/Portfolio/MyPortfolio.java
@@ -47,4 +47,53 @@ public class MyPortfolio {
                 """;
     }
 
+    @GetMapping("/projects")
+    public String myProjects(){
+        return """
+                <h1> Projects </h1> 
+                
+                <h4> Title: <em>InterviewHub </em> | React.js, Node.js, Express.js, PostgreSQL, Prisma ORM, Docker, Docker Compose, GitHub Actions</h4>  
+                <ul>
+                <li> 
+                Orchestrated a full-stack interview experience platform for exploring and sharing company-wise and role-wise interview
+                experiences.
+                </li>
+                <li>
+                Modeled relational data structures using PostgreSQL and Prisma ORM to manage users, companies, job roles, experiences,\s
+                and interview rounds.\s
+                </li>
+                <li>
+                 Containerized the app with Docker and Docker Compose, simplifying multi-container deployment and environment consistency.
+                </li>
+                
+                <li>
+                 Automated CI workflows with GitHub Actions for application builds, Docker image creation, and image publishing to Docker Hub.
+                </li>
+                </ul>
+                
+                <br/>
+                
+                <h4> Title: <em>Virtual Classroom Platform </em> |  MongoDB, Express.js, React.js, Node.js, WebRTC, Tailwind CSS\s</h4>  
+                <ul>
+                <li> 
+                 Architected a real-time virtual classroom using WebRTC and Socket.io, supporting up to 10 concurrent users with low-latency\s
+                            audio/video communication per session.\s
+                </li>
+                <li>
+                Engineered an assignment workflow handling 30+ student submissions per course, including file uploads, deadline tracking, and\s
+                instructor reviews.\s
+                </li>
+                <li>
+                 Secured 20+ learning-content resources using role-based access control enforcing permissions across 2 user roles: students\s
+                                                                                                                                          and instructors.\s
+                </li>
+                
+                <li>
+                 Developed dual dashboards for 2 user roles, organizing classes, tasks, announcements and shared materials in one platform.\s
+                </li>
+                </ul>
+                
+                
+                """;
+    }
 }
