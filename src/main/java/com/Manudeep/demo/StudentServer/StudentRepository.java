5e6b65ab6d8a9481829f4327fc23241a7d7e19bd
manudeep <manudeep1000@gmail.com>
2026-07-15 11:34:15 +0530

    Spring data jpa

--- src/main/java/com/Manudeep/demo/StudentServer/StudentRepository.java
+++ src/main/java/com/Manudeep/demo/StudentServer/StudentRepository.java
@@ -1,13 +1,14 @@
 package com.Manudeep.demo.StudentServer;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentRepository {
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 
-    public Student save(Student student){
-        System.out.println("Student Information saved");
-        return student;
-    }
+//    public Student save(Student student){
+//        System.out.println("Student Information saved");
+//        return student;
+//    }
 
 }
